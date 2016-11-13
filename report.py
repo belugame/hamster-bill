c2bccc55be05e2fcd8a7a5e62eb6a1402709182f
Martin Bauer <mb@altmuehl.net>
2016-11-13 23:00:35 +0100

    monthly report

--- report.py
+++ report.py
@@ -5,7 +5,9 @@ import os
 import operator
 from calendar import monthrange
 from collections import defaultdict
+from time import strptime
 
+from ascii_graph import Pyasciigraph
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
@@ -16,25 +18,27 @@ from reader import HamsterDBReader
 class HamsterReport:
     hours_after_midnite_to_include = 4
 
-    def __init__(self, client, month, year):
+    def __init__(self, client, year, month=None):
         assert client in config.CUSTOMER.keys(), \
             "Unknown customer '{}', please add it to config.py first.".format(client)
         self.client = client
         self.month = month
         self.year = year
         self.billing_period = self.determine_billing_period()
-
         with HamsterDBReader(config.HAMSTER_DB) as db:
             self.facts = db.get_facts(*self.billing_period, self.client)
 
-
     def determine_billing_period(self):
         """Include activities of following day until 4am"""
-        start_time = datetime.datetime(self.year, self.month, 1, 0, 0, 0)
-        _, last = monthrange(self.year, self.month)
-        end_time = start_time + datetime.timedelta(days=last, hours=self.hours_after_midnite_to_include)
-        now = datetime.datetime.now()
-        end_time = end_time if now > end_time else now
+        if self.month:  # 1 month
+            start_time = datetime.datetime(self.year, self.month, 1, 0, 0, 0)
+            _, last = monthrange(self.year, self.month)
+            end_time = start_time + datetime.timedelta(days=last, hours=self.hours_after_midnite_to_include)
+            now = datetime.datetime.now()
+            end_time = end_time if now > end_time else now
+        else:  # whole year
+            start_time = datetime.datetime(self.year, 1, 1, 0, 0, 0)
+            end_time = datetime.datetime(self.year + 1, 1, 1, self.hours_after_midnite_to_include, 0, 0)
         return start_time, end_time
 
     def get_durations_by_activity(self, facts):
@@ -57,36 +61,51 @@ class HamsterReport:
         days = sorted(days.items(), key=operator.itemgetter(0))
         return days, total
 
+    def get_durations_by_month(self, facts):
+        months = defaultdict(datetime.timedelta)
+        for f in facts:
+            months[f.start_time.date().strftime("%B")] += f.duration
+        total = sum([d.total_seconds() for d in months.values()])/3600
+        months = sorted(months.items(), key=lambda x: strptime(x[0], "%B"))
+        return months, total
+
+    def make_monthly_report(self):
+        total_duration = datetime.timedelta()
+        months, total_hours = self.get_durations_by_month(self.facts)
+        title = 'Total hours in {}: {:.2f}'.format(self.year, total_hours)
+        self.make_bar_chart(months, title)
+
     def make_daily_report(self):
         total_duration = datetime.timedelta()
         days, total_hours = self.get_durations_by_day(self.facts)
-        self.make_bar_chart(days)
+        title = 'Total hours in {}/{}: {:.2f}'.format(self.month, self.year, total_hours)
+        self.make_bar_chart(days, title)
 
-    def make_bar_chart(self, days):
-        from ascii_graph import Pyasciigraph
-        graph = Pyasciigraph(float_format='{0:,.2f}')
-
-        items = [(str(date), timedelta.total_seconds()/3600) for date, timedelta in days]
-        for line in graph.graph('{}/{}'.format(self.month, self.year), items):
+    def make_bar_chart(self, data, title):
+        items = [(str(date), timedelta.total_seconds()/3600) for date, timedelta in data]
+        graph = Pyasciigraph(float_format='{0:,.2f}', graphsymbol='|')
+        for line in graph.graph(title, items):
             print(line)
-        # for day, duration in days:
-            # print("{}  {:6.2f} h".format(day, duration.total_seconds()/3600))
-        # print("---------------------")
-        # print("Total:       {:.2f} h in {}/{}".format(total_hours,
-                                                      # self.month, self.year))
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(add_help=False)
-    parser.add_argument('yearmonth', nargs="?")
+    parser.add_argument('period', nargs="?")
     parser.add_argument('client')  # = hamster tag
     args = parser.parse_args()
 
     client = args.client
-    if args.yearmonth:
-        year, month = (int(i) for i in args.yearmonth.split("-"))
+    month = None
+    if args.period:
+        if "-" in args.period:
+            year, month = (int(i) for i in args.period.split("-"))
+        else:
+            year = int(args.period)
     else:
         today = datetime.date.today()
         year, month = today.year, today.month
 
-    HamsterReport(client, month, year).make_daily_report()
+    if month:
+        HamsterReport(client, year, month).make_daily_report()
+    else:
+        HamsterReport(client, year).make_monthly_report()
